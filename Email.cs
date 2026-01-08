a6aaa4cc61e083fbd7349c2c29bfc39af5369ae9
track-frags <jadirks@anamosa.k12.ia.us>
2026-01-08 16:08:45 +0000

    thjis

--- Email.cs
+++ Email.cs
@@ -1,5 +1,8 @@
 ﻿namespace knightmoves;
 public class Email
 {
-    // Add your code here
+    public const string introduction = "Dear Jacob, \n" +
+    "\tThank you for being such a \"wonderful\" manager, \n" +
+    "Sincerly,\n" +
+    "Jack :\\";
 }
